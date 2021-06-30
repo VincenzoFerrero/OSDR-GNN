2853bc582857f5b8ba1d275ef531ba942eeafbbf
Daniele Grandi <daniele.grandi@autodesk.com>
2021-06-30 16:13:48 -0700

    code for paper feb 12

--- GNN/dataset.py
+++ GNN/dataset.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 import networkx as nx
 import torch.nn.functional as F
 from collections import Counter
@@ -8,7 +9,7 @@ from sklearn.model_selection import train_test_split
 
 
 def get_vocab():
-    graphs = load_data('data/autodesk_colab_fullv3_202010291746.csv')
+    graphs = load_data('../data/autodesk_colab_fullv3_202010291746.csv')
     subfunc_counts, tier1_counts, tier2_counts, tier3_counts = [], [], [], []
     vocab = {
         'component_basis': set(),
@@ -67,58 +68,199 @@ def get_vocab():
     return graphs, vocab, weights
 
 
-def preprocess():
+def degree_encoding(max_degree=100, dimension=16):
+    deg_enc = np.array([
+        [pos / np.power(10000, 2 * (j // 2) / dimension) for j in range(dimension)]
+        if pos != 0 else np.zeros(dimension) for pos in range(max_degree + 1)])
+    deg_enc[1:, 0::2] = np.sin(deg_enc[1:, 0::2])
+    deg_enc[1:, 1::2] = np.cos(deg_enc[1:, 1::2])
+    return deg_enc
+
+
+def draw_class():
+    import seaborn as sn
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    from collections import Counter
+
+    graphs = load_data('../data/autodesk_colab_fullv3_202010291746.csv')
+    tier1_counts, tier2_counts, tier3_counts = [], [], []
+
+    for g in graphs:
+        for __, attr in g.nodes(data=True):
+            tier1_counts.append(attr['tier_1_function'])
+            tier2_counts.append(attr['tier_2_function'])
+            tier3_counts.append(attr['tier_3_function'])
+
+    f, l = [], []
+    for k, v in Counter(tier1_counts).most_common():
+        if k:
+            f.append(v/len(tier1_counts))
+            l.append(k)
+    d = pd.DataFrame({'Frequency': f, 'Class': l})
+    plt.figure(figsize=(12, 9))
+    sn.barplot(data=d, y='Class', x='Frequency', color='cyan')
+    plt.xlabel('Frequency', size='xx-large')
+    plt.ylabel('Class', size='xx-large')
+    plt.xticks(size='xx-large')
+    plt.yticks(size='xx-large')
+    plt.tight_layout()
+    plt.savefig(fname='logs/tier1_freq.pdf', format='pdf')
+    plt.show()
+
+    f, l = [], []
+    for k, v in Counter(tier2_counts).most_common():
+        if k:
+            f.append(v/len(tier2_counts))
+            l.append(k)
+    d = pd.DataFrame({'Frequency': f, 'Class': l})
+    plt.figure(figsize=(13, 10))
+    sn.barplot(data=d, y='Class', x='Frequency', color='cyan')
+    plt.xlabel('Frequency', size='xx-large')
+    plt.ylabel('Class', size='xx-large')
+    plt.xticks(size='xx-large')
+    plt.yticks(size='xx-large')
+    plt.tight_layout()
+    plt.savefig(fname='logs/tier2_freq.pdf', format='pdf')
+    plt.show()
+
+    f, l = [], []
+    for k, v in Counter(tier3_counts).most_common():
+        if k:
+            f.append(v/len(tier3_counts))
+            l.append(k)
+    d = pd.DataFrame({'Frequency': f, 'Class': l})
+    plt.figure(figsize=(13, 10))
+    sn.barplot(data=d, y='Class', x='Frequency', color='cyan')
+    plt.xlabel('Frequency', size='xx-large')
+    plt.ylabel('Class', size='xx-large')
+    plt.xticks(size='xx-large')
+    plt.yticks(size='xx-large')
+    plt.tight_layout()
+    plt.savefig(fname='logs/tier3_freq.pdf', format='pdf')
+    plt.show()
+
+
+def preprocess(node_feature, edge_feature):
     graphs = []
     data, vocab, weights = get_vocab()
+
+    max_degree = max([max(dict(nx.degree(graph)).values()) for graph in data])
 
     for graph in data:
         if graph.number_of_nodes() < 3 or graph.number_of_edges() < 2:
             continue
-
+        nodes, edges = [], []
         mappings = {n: idx for idx, n in enumerate(graph.nodes())}
         graph = nx.relabel_nodes(graph, mappings)
 
-        nodes = torch.cat((
-            F.one_hot(
-                torch.tensor([vocab['component_basis'][n[-1]['component_basis']] for n in graph.nodes(data=True)]),
-                len(vocab['component_basis'])),
-            F.one_hot(
+        if node_feature == 'none':
+            deg_enc = degree_encoding(max_degree)
+            nodes = torch.tensor([deg_enc[graph.degree[n[0]]] for n in graph.nodes(data=True)])
+        elif node_feature == 'component':
+            nodes = F.one_hot(
+                    torch.tensor([vocab['component_basis'][n[-1]['component_basis']] for n in graph.nodes(data=True)]),
+                    len(vocab['component_basis']))
+        elif node_feature == 'name':
+            nodes = F.one_hot(
                 torch.tensor([vocab['sys_name'][n[-1]['sys_name']] for n in graph.nodes(data=True)]),
-                len(vocab['sys_name'])),
-            F.one_hot(
+                len(vocab['sys_name']))
+        elif node_feature == 'type':
+            nodes = F.one_hot(
                 torch.tensor([vocab['sys_type_name'][n[-1]['sys_type_name']] for n in graph.nodes(data=True)]),
-                len(vocab['sys_type_name'])),
-            F.one_hot(
+                len(vocab['sys_type_name']))
+        elif node_feature == 'material':
+            nodes = F.one_hot(
                 torch.tensor([vocab['material_name'][n[-1]['material_name']] for n in graph.nodes(data=True)]),
-                len(vocab['material_name'])),
-        ), -1)
+                len(vocab['material_name']))
+        elif node_feature == 'all':
+            nodes = torch.cat((
+                F.one_hot(
+                    torch.tensor([vocab['component_basis'][n[-1]['component_basis']] for n in graph.nodes(data=True)]),
+                    len(vocab['component_basis'])),
+                F.one_hot(
+                    torch.tensor([vocab['sys_name'][n[-1]['sys_name']] for n in graph.nodes(data=True)]),
+                    len(vocab['sys_name'])),
+                F.one_hot(
+                    torch.tensor([vocab['sys_type_name'][n[-1]['sys_type_name']] for n in graph.nodes(data=True)]),
+                    len(vocab['sys_type_name'])),
+                F.one_hot(
+                    torch.tensor([vocab['material_name'][n[-1]['material_name']] for n in graph.nodes(data=True)]),
+                    len(vocab['material_name'])),
+            ), -1)
 
         y = torch.tensor([vocab[f'subfunction_basis'][n[-1][f'subfunction_basis']] for n in graph.nodes(data=True)])
         y1 = torch.tensor([vocab[f'tier_1_function'][n[-1][f'tier_1_function']] for n in graph.nodes(data=True)])
         y2 = torch.tensor([vocab[f'tier_2_function'][n[-1][f'tier_2_function']] for n in graph.nodes(data=True)])
         y3 = torch.tensor([vocab[f'tier_3_function'][n[-1][f'tier_3_function']] for n in graph.nodes(data=True)])
 
-        edges = []
-        for edge in graph.edges(data=True):
-            edges.append(torch.zeros(2 * len(vocab['flow']) + 1))
-            if len(edge[-1]) == 0:
-                edges[-1][0] = 1.
-            else:
-                if 'input_flow' in edge[-1]:
-                    edges[-1][vocab['flow'][edge[-1]['input_flow']] + 1] = 1.
-                if 'output_flow' in edge[-1]:
-                    edges[-1][vocab['flow'][edge[-1]['output_flow']] + len(vocab['flow']) + 1] = 1.
+        if edge_feature == 'all':
+            for edge in graph.edges(data=True):
+                edges.append(torch.zeros(2 * len(vocab['flow']) + 1))
+                if len(edge[-1]) == 0:
+                    edges[-1][0] = 1.
+                else:
+                    if 'input_flow' in edge[-1]:
+                        edges[-1][vocab['flow'][edge[-1]['input_flow']] + 1] = 1.
+                    if 'output_flow' in edge[-1]:
+                        edges[-1][vocab['flow'][edge[-1]['output_flow']] + len(vocab['flow']) + 1] = 1.
+            edges = torch.stack(edges)
+            edge_index = torch.tensor([[e[0], e[1]] for e in graph.edges()]).transpose(1, 0)
+        elif edge_feature == 'flow':
+            try:
+                edge_index = \
+                    torch.tensor([[e[0], e[1]] for e in graph.edges(data=True) if len(e[-1]) > 0]).transpose(1, 0)
+            except:
+                continue
+            for edge in graph.edges(data=True):
+                if len(edge[-1]) > 0:
+                    edges.append(torch.zeros(2 * len(vocab['flow'])))
+                    if 'input_flow' in edge[-1]:
+                        edges[-1][vocab['flow'][edge[-1]['input_flow']]] = 1.
+                    if 'output_flow' in edge[-1]:
+                        edges[-1][vocab['flow'][edge[-1]['output_flow']] + len(vocab['flow'])] = 1.
+            edges = torch.stack(edges)
+        elif edge_feature == 'solid':
+            try:
+                edge_index = torch.tensor([[e[0], e[1]] for e in graph.edges(data=True)
+                                           if len(e[-1]) > 0 and
+                                           (('input_flow' in e[-1] and e[-1]['input_flow'] == 'solid') or
+                                            ('output_flow' in e[-1] and e[-1]['output_flow'] == 'solid'))])\
+                    .transpose(1, 0)
+            except:
+                continue
+            for edge in graph.edges(data=True):
+                if len(edge[-1]) > 0:
+                    e = torch.zeros(2)
+                    if 'input_flow' in edge[-1] and edge[-1]['input_flow'] == 'solid':
+                        e[0] = 1.
+                    if 'output_flow' in edge[-1] and edge[-1]['output_flow'] == 'solid':
+                        e[1] = 1.
+                    if torch.sum(e) > 0:
+                        edges.append(e)
+            edges = torch.stack(edges)
+        elif edge_feature == 'assembly':
+            try:
+                edge_index = \
+                    torch.tensor([[e[0], e[1]] for e in graph.edges(data=True) if len(e[-1]) == 0]).transpose(1, 0)
+            except:
+                continue
+            for edge in graph.edges(data=True):
+                if len(edge[-1]) == 0:
+                    edges.append(torch.ones(5))
+            edges = torch.stack(edges)
+        elif edge_feature == 'none':
+            edges = torch.ones((nx.number_of_edges(graph), 5))
+            edge_index = torch.tensor([[e[0], e[1]] for e in graph.edges()]).transpose(1, 0)
 
-        edges = torch.stack(edges)
-        edge_index = torch.tensor([[e[0], e[1]] for e in graph.edges()]).transpose(1, 0)
         graphs.append(Data(x=nodes, edge_index=edge_index, e=edges, y=y, y1=y1, y2=y2, y3=y3))
 
     return graphs, vocab, weights
 
 
 class DataSet(object):
-    def __init__(self, batch_size):
-        self.graphs, self.vocab, self.weight = preprocess()
+    def __init__(self, batch_size, node_feature, edge_feature):
+        self.graphs, self.vocab, self.weight = preprocess(node_feature, edge_feature)
         self.batch_size = batch_size
         self.node_dim = self.graphs[0].x.shape[-1]
         self.edge_dim = self.graphs[0].e.shape[-1]
@@ -131,7 +273,7 @@ class DataSet(object):
         self.shuffle()
 
     def shuffle(self):
-        train, test = train_test_split(self.graphs, test_size=.4, shuffle=True)
+        train, test = train_test_split(self.graphs, test_size=.3, shuffle=True)
         train, val = train_test_split(self.graphs, test_size=.1, shuffle=True)
         self.train_loader = DataLoader(train, batch_size=self.batch_size, shuffle=True)
         self.val_loader = DataLoader(val, batch_size=self.batch_size, shuffle=False)
@@ -139,5 +281,4 @@ class DataSet(object):
 
 
 if __name__ == '__main__':
-    graphs, vocab, w = preprocess(1)
-    print('heer')
+    draw_class()
--- GNN/train.py
+++ GNN/train.py
@@ -8,13 +8,14 @@ import torch.nn as nn
 from tqdm import tqdm
 from dataset import DataSet
 from itertools import product
-from gnn import HierarchicalGNN
+from gnn import HierarchicalGNN, MLP, Linear
 from torch.nn import functional as F
-from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix
 
 
 class HierarchicalClassifier(object):
     def __init__(self, args):
+        self.verbose = args.verbose
         self.device = torch.device(args.device)
         self.batch_size = args.batch_size
         self.num_epochs = args.num_epochs
@@ -28,35 +29,47 @@ class HierarchicalClassifier(object):
         self.hid_dim = args.hid_dim
         self.lr = args.lr
 
-        self.gnn = HierarchicalGNN(self.node_dim, self.edge_dim, self.hid_dim, self.num_class_l1,
-                                   self.num_class_l2, self.num_class_l3, self.num_layers, args.network).to(self.device)
+        if args.network == 'mlp':
+            self.model = MLP(self.node_dim, self.hid_dim, self.num_class_l1,
+                             self.num_class_l2, self.num_class_l3).to(self.device)
+        elif args.network == 'linear':
+            self.model = Linear(self.node_dim, self.hid_dim, self.num_class_l1,
+                                self.num_class_l2, self.num_class_l3).to(self.device)
+        else:
+            self.model = HierarchicalGNN(self.node_dim, self.edge_dim, self.hid_dim,
+                                         self.num_class_l1, self.num_class_l2, self.num_class_l3,
+                                         self.num_layers, args.network).to(self.device)
 
     def load(self):
         if os.path.exists('checkpoint.pkl'):
-            self.gnn.load_state_dict(torch.load('checkpoint.pkl'))
+            self.model.load_state_dict(torch.load('checkpoint.pkl'))
         else:
             raise Exception('Checkpoint not found ...')
 
     def train(self, train_loader, val_loader, weights):
-        best_loss, best_state, patience_count = 1e9, None, 0
-        optimizer = torch.optim.Adam(self.gnn.parameters(), lr=self.lr)
+        best_loss, best_state, patience_count = 1e9, self.model.state_dict(), 0
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=self.num_epochs)
         weights_l1 = weights[1].to(self.device)
         weights_l2 = weights[2].to(self.device)
         weights_l3 = weights[3].to(self.device)
 
         for epoch in range(self.num_epochs):
-            self.gnn.train()
+            self.model.train()
             epoch_loss = 0.
             start = time.time()
             for batch in train_loader:
                 batch = batch.to(self.device)
                 optimizer.zero_grad()
 
-                logits_l1, logits_l2, logits_l3 = self.gnn(
-                    batch.x.float(), batch.edge_index, batch.e.float(),
-                    F.one_hot(batch.y1, self.num_class_l1),
-                    F.one_hot(batch.y2, self.num_class_l2))
+                if args.network in ('mlp', 'linear'):
+                    logits_l1, logits_l2, logits_l3 = self.model(
+                        batch.x.float(), F.one_hot(batch.y1, self.num_class_l1), F.one_hot(batch.y2, self.num_class_l2))
+                else:
+                    logits_l1, logits_l2, logits_l3 = self.model(
+                        batch.x.float(), batch.edge_index, batch.e.float(),
+                        F.one_hot(batch.y1, self.num_class_l1),
+                        F.one_hot(batch.y2, self.num_class_l2))
 
                 is_labeled = batch.y1 > 0
                 loss1 = nn.CrossEntropyLoss(weight=weights_l1)(logits_l1[is_labeled], batch.y1[is_labeled])
@@ -73,26 +86,28 @@ class HierarchicalClassifier(object):
             end = time.time()
             val_loss, _, _, _, _, _, _ = self.predict(val_loader)
 
-            # print(f'Epoch: {epoch + 1:03d}/{self.num_epochs}, Time: {end-start:.2f}s, '
-            #       f'Train Loss: {epoch_loss / len(train_loader):.4f}, Val Loss: {val_loss: .4f}')
+            if self.verbose:
+                print(f'Epoch: {epoch + 1:03d}/{self.num_epochs}, Time: {end-start:.2f}s, '
+                      f'Train Loss: {epoch_loss / len(train_loader):.4f}, Val Loss: {val_loss: .4f}')
 
             if best_loss > val_loss:
                 best_loss = val_loss
-                best_state = self.gnn.state_dict()
+                best_state = self.model.state_dict()
                 patience_count = 0
             else:
                 patience_count += 1
 
             if patience_count == self.patience:
-                # print('Early stopping ...')
+                if self.verbose:
+                    print('Early stopping ...')
                 break
 
-        self.gnn.load_state_dict(best_state)
+        self.model.load_state_dict(best_state)
         torch.save(best_state, 'checkpoint.pkl')
 
     @torch.no_grad()
     def predict(self, data_loader):
-        self.gnn.eval()
+        self.model.eval()
 
         loss = 0.
         yp_l1, yp_l2, yp_l3 = [], [], []
@@ -100,7 +115,10 @@ class HierarchicalClassifier(object):
 
         for batch in data_loader:
             batch = batch.to(self.device)
-            logits_l1, logits_l2, logits_l3 = self.gnn.predict(batch.x.float(), batch.edge_index, batch.e.float())
+            if args.network in ('mlp', 'linear'):
+                logits_l1, logits_l2, logits_l3 = self.model.predict(batch.x.float())
+            else:
+                logits_l1, logits_l2, logits_l3 = self.model.predict(batch.x.float(), batch.edge_index, batch.e.float())
             is_labeled = batch.y1 > 0
             loss1 = nn.CrossEntropyLoss()(logits_l1[is_labeled], batch.y1[is_labeled])
             is_labeled = batch.y2 > 0
@@ -128,7 +146,7 @@ class HierarchicalClassifier(object):
 
 
 def cross_validate(args):
-    dataset = DataSet(args.batch_size)
+    dataset = DataSet(args.batch_size, args.node_feature, args.edge_feature)
     args.node_dim = dataset.node_dim
     args.edge_dim = dataset.edge_dim
     args.num_class_l1 = dataset.num_class_l1
@@ -243,22 +261,98 @@ def cross_validate(args):
         json.dump({**args.__dict__, **result}, f, indent=2)
 
 
+def draw_confusion(args):
+    import seaborn as sn
+    import matplotlib.pyplot as plt
+    dataset = DataSet(args.batch_size, args.node_feature, args.edge_feature)
+    args.node_dim = dataset.node_dim
+    args.edge_dim = dataset.edge_dim
+    args.num_class_l1 = dataset.num_class_l1
+    args.num_class_l2 = dataset.num_class_l2
+    args.num_class_l3 = dataset.num_class_l3
+
+    yp_l1_all, yp_l2_all, yp_l3_all = [], [], []
+    yt_l1_all, yt_l2_all, yt_l3_all = [], [], []
+
+    for __ in tqdm(range(100), unit_scale=True, desc='Running experiments...'):
+        classifier = HierarchicalClassifier(args)
+        classifier.train(dataset.train_loader, dataset.val_loader, dataset.weight)
+        loss, yp_l1, yp_l2, yp_l3, yt_l1, yt_l2, yt_l3 = classifier.predict(dataset.test_loader)
+        yp_l1_all.append(yp_l1[yt_l1 > 0].cpu().numpy())
+        yp_l2_all.append(yp_l2[yt_l2 > 0].cpu().numpy())
+        yp_l3_all.append(yp_l3[yt_l3 > 0].cpu().numpy())
+        yt_l1_all.append(yt_l1[yt_l1 > 0].cpu().numpy())
+        yt_l2_all.append(yt_l2[yt_l2 > 0].cpu().numpy())
+        yt_l3_all.append(yt_l3[yt_l3 > 0].cpu().numpy())
+        torch.cuda.empty_cache()
+        dataset.shuffle()
+
+    yt_l1_all = np.concatenate(yt_l1_all)
+    yp_l1_all = np.concatenate(yp_l1_all)
+    yt_l2_all = np.concatenate(yt_l2_all)
+    yp_l2_all = np.concatenate(yp_l2_all)
+    yt_l3_all = np.concatenate(yt_l3_all)
+    yp_l3_all = np.concatenate(yp_l3_all)
+
+    cf_l1 = confusion_matrix(yt_l1_all[yp_l1_all > 0], yp_l1_all[yp_l1_all > 0], normalize='true')
+    cf_l2 = confusion_matrix(yt_l2_all[yp_l2_all > 0], yp_l2_all[yp_l2_all > 0], normalize='true')
+    cf_l3 = confusion_matrix(yt_l3_all[yp_l3_all > 0], yp_l3_all[yp_l3_all > 0], normalize='true')
+
+    if not os.path.exists('logs/'):
+        os.makedirs('logs/')
+
+    plt.figure(figsize=(12, 9))
+    label = list(dataset.vocab['tier_1_function'].keys())[1:]
+    sn.heatmap(cf_l1, annot=False, fmt='.2f', cmap='Blues', xticklabels=label, yticklabels=label)
+    plt.xticks(size='xx-large', rotation=45)
+    plt.yticks(size='xx-large', rotation=45)
+    plt.tight_layout()
+    plt.savefig(fname='logs/tier1.pdf', format='pdf')
+    plt.show()
+
+    plt.figure(figsize=(12, 9))
+    label = list(dataset.vocab['tier_2_function'].keys())[1:]
+    sn.heatmap(cf_l2, annot=False, fmt='.2f', cmap='Blues', xticklabels=label, yticklabels=label)
+    plt.xticks(size='xx-large')
+    plt.yticks(size='xx-large')
+    plt.tight_layout()
+    plt.savefig(fname='logs/tier2.pdf', format='pdf')
+    plt.show()
+
+    plt.figure(figsize=(12, 9))
+    label = list(dataset.vocab['tier_3_function'].keys())[1:]
+    sn.heatmap(cf_l3, annot=False, fmt='.2f', cmap='Blues', xticklabels=label, yticklabels=label)
+    plt.xticks(size='xx-large')
+    plt.yticks(size='xx-large')
+    plt.tight_layout()
+    plt.savefig(fname='logs/tier3.pdf', format='pdf')
+    plt.show()
+
+
 def search(args):
-    grid = [[64, 128, 256], [1, 2, 3]]
-    for c in product(*grid):
-        args.hid_dim = c[0]
-        args.num_layers = c[1]
-        cross_validate(args)
+    if args.network in ('mlp', 'linear'):
+        for h in [64, 128, 256]:
+            args.hid_dim = h
+            cross_validate(args)
+    else:
+        grid = [[64, 128, 256], [1, 2, 3]]
+        for c in product(*grid):
+            args.hid_dim = c[0]
+            args.num_layers = c[1]
+            cross_validate(args)
 
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--network', type=str, default='gin', choices=['gcn', 'gat', 'gin', 'sage'])
+    parser.add_argument('--network', type=str, default='sage', choices=['gcn', 'gat', 'gin', 'sage', 'mlp', 'linear'])
+    parser.add_argument('--node_feature', type=str, default='all',
+                        choices=['all', 'none', 'component', 'name', 'type', 'material'])
+    parser.add_argument('--edge_feature', type=str, default='all', choices=['all', 'none', 'flow', 'assembly'])
     parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--patience', type=int, default=50)
-    parser.add_argument('--hid_dim', type=int, default=64)
+    parser.add_argument('--hid_dim', type=int, default=256)
     parser.add_argument('--num_epochs', type=int, default=1000)
-    parser.add_argument('--num_layers', type=int, default=2)
+    parser.add_argument('--num_layers', type=int, default=3)
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--device', type=str, default='cuda:3')
     parser.add_argument('--node_dim', type=int)
@@ -266,6 +360,7 @@ def get_parser():
     parser.add_argument('--num_class_l1', type=int)
     parser.add_argument('--num_class_l2', type=int)
     parser.add_argument('--num_class_l3', type=int)
+    parser.add_argument('--verbose', type=bool, default=False)
     return parser.parse_args()
 
 
